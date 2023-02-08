1f81bffe159a4e3decfe55c47ba1d31cac38467a
tkgandhi <109563744+tkgandhi@users.noreply.github.com>
2023-02-07 20:52:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// https://teachablemachine.withgoogle.com/models/4SmH1xiP1/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+ data_uri+'"/>';
+ });
+
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/WbKt6MHv7/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log('modelLoaded');
+}
